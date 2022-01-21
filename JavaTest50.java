9a7f1c9d0d5f7b0d37af45a4ab0df74b6ea150ac
Shimcoding <91776742+Shimcoding@users.noreply.github.com>
2022-01-21 13:16:21 +0900

    java_jungsuk3_연습문제 3-5
    아래는 변수 의 값 중에서 일의 자리를 1로 바꾸는 코드이다.  만일 변수 num의 값이 333이라면 331이 되고, 777이라면 771이 된다. 
    (1)에 알맞은 코드를 넣으시오.
    
    [ ] 연습문제 /ch3/Exercise3_5.java
    class Exercise3_5 {
    public static void main(String[] args) {
    int num = 333;
    System.out.println( /* (1) */ );
    }
    }
    
    [실행결과]
    331

---
+++ JavaTest50.java
@@ -0,0 +1,77 @@
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class JavaTest50 {
+	
+	public static void main(String[] args){
+		
+		// 1.값이 주어졌을 때
+//		int num = 333;
+//		System.out.println(num - (num%10-1));
+		
+		
+		
+		// 2.두 자리 수 이상 값을 사용자로부터 입력 받을 때
+//		Scanner sc = new Scanner(System.in);
+//		while(true) {
+//			System.out.print("두 자리 수 이상인 수를 입력(q:종료)>>");
+//			String input = sc.nextLine().trim();
+//			
+//			if(input.equals("q")) {
+//				System.out.println("프로그램을 종료합니다.");
+//				break;
+//			}
+//			if(input.length()<2) {
+//				System.out.println("두 자리 수 이상인 수가 아닙니다.");
+//				continue;
+//			} else {
+//				int num = 0;
+//				try {
+//					num = Integer.parseInt(input);
+//				} catch (Exception e) {
+//					System.out.println("숫자로 변환할 수 없습니다.");
+//					continue;
+//				}
+//				
+//				System.out.println("값: "+(num-(num%10-1)));
+//			}
+//			
+//		}		
+		
+		
+		
+		// 3.맨 앞 자리를 1로 바꿀 때
+//		Scanner sc = new Scanner(System.in);
+//		while(true) {
+//			System.out.print("두 자리 수 이상인 수를 입력(q:종료)>>");
+//			String input = sc.nextLine().trim();
+//			
+//			if(input.equals("q")) {
+//				System.out.println("프로그램을 종료합니다.");
+//				break;
+//			}
+//			if(input.length()<2) {
+//				System.out.println("두 자리 수 이상인 수가 아닙니다.");
+//				continue;
+//			} else {
+//				int num = 0;
+//				try {
+//					num = Integer.parseInt(input);
+//				} catch (Exception e) {
+//					System.out.println("숫자로 변환할 수 없습니다.");
+//					continue;
+//				}
+//				if(input.charAt(0) != '1') {
+//					input = input.replaceFirst(input.charAt(0)+"","1");
+//				}
+//				System.out.println("값: "+input);
+//			}
+//			
+//		}	
+		
+		
+	}
+}
