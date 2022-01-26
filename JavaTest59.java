82d41476b6b19499388c49bdfc7c32f9990be778
Shimcoding <91776742+Shimcoding@users.noreply.github.com>
2022-01-26 12:01:08 +0900

    java_jungsuk3_연습문제 4-11(피보나치 수열 구하기)
    피보나치 수열을 구하기(첫 번째, 두 번째, 세 번째 값이 주어짐.)
    
    public class Exercise4_11 {
    public static void main(String[] args) {
    // Fibonnaci 1, 1 . 수열의 시작의 첫 두 숫자를 로 한다
    int num1 = 1;
    int num2 = 1;
    int num3 = 0; // 세번째 값
    System.out.print(num1+","+num2);
    for (int i = 0 ; i < 8 ; i++ ) {
    /*
    (1) . 알맞은 코드를 넣어 완성하시오
    */
    }
    } // end of main
    } // end of class
    
    [ 실행결과 ]
    1,1,2,3,5,8,13,21,34,55

---
+++ JavaTest59.java
@@ -0,0 +1,70 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import javax.sound.midi.Instrument;
+import javax.sound.midi.MidiEvent;
+
+
+public class JavaTest59 {
+	
+	public static void main(String[] args){
+		// 1.피보나치 수열
+//		int num1 = 1;
+//		int num2 = 1;
+//		int num3 = 0; // 세번째 값
+//		System.out.print(num1+","+num2);
+//		
+//		for (int i = 0 ; i < 8 ; i++ ) {
+//			
+//			num3 = num1 + num2;
+//			System.out.print(","+num3);
+//			num1 = num2;
+//			num2 = num3;
+//		}
+
+		
+		
+		
+		// 2.while문으로 피보나치 수열(10번째까지 구하기)
+//		final int MAX_NUM = 10;
+//		int[] arr = new int[MAX_NUM];
+//		
+//		fibo(arr,MAX_NUM);
+//		
+//		System.out.println(Arrays.toString(arr));
+	}
+
+//	private static void fibo(int[] arr, int num) {
+//		int i = 0;
+//		
+//		while(arr[num-1] == 0) {
+//			if(i==0) {
+//				arr[i] = 1;
+//			}else if(i==1) {
+//				arr[i] = 1;
+//			}else {
+//				arr[i] = arr[i-1]+arr[i-2];
+//			}
+//			i++;
+//		}
+//		
+//	}
+}
+
+
+
+
+
