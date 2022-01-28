081e7051a6252be7ce56047ce9280bc0ae03b5b5
Shimcoding <91776742+Shimcoding@users.noreply.github.com>
2022-01-28 15:19:38 +0900

    java_jungsuk3_연습문제 5-3(배열의 요소 합 구하기)
    배열 arr에 담긴 모든 값을 더하는 프로그램을 완성하시오.
    
    class Exercise5_3
    {
    public static void main(String[] args)
    {
    int[] arr = {10, 20, 30, 40, 50};
    int sum = 0;
    /*
    (1) . 알맞은 코드를 넣어 완성하시오
    */
    System.out.println("sum="+sum);
    }
    }
    
    [실행결과]
    sum=150

---
+++ JavaTest63.java
@@ -0,0 +1,53 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import javax.sound.midi.Instrument;
+import javax.sound.midi.MidiEvent;
+
+public class JavaTest63 {
+
+	public static void main(String[] args) {
+		// 1. for문을 이용해서 합 구하기
+//		int[] arr = {10, 20, 30, 40, 50};
+//		int sum = 0;
+//		
+//		for (int i = 0; i < arr.length; i++) {
+//			sum += arr[i];
+//		}
+//		
+//		System.out.println("sum="+sum);
+
+		
+		
+		// 2. 스트림을 이용해서 합계 및 평균 구하기
+//		int[] arr = {10, 20, 30, 40, 50};
+//		System.out.println("sum="+ Arrays.stream(arr).sum());
+//		System.out.println("avg="+ Arrays.stream(arr).average());
+	
+		
+		
+		// 3. 배열을 리스트로 바꿔서 합계 구하기
+//		Integer[] arr = {10, 20, 30, 40, 50};
+//		List<Integer> list = Arrays.asList(arr);
+//		int sum = 0;
+//		
+//		for (int i = 0; i < list.size(); i++) {
+//			sum += list.get(i);
+//		}
+//		
+//		System.out.println("sum="+sum);
+	}
+}
