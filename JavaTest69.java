0c854cf5002018a1cac38785ebaa8ec2b7c6cd47
Shimcoding <91776742+Shimcoding@users.noreply.github.com>
2022-02-01 15:48:33 +0900

    java_jungsuk3_연습문제 5-9(주어진 배열 회전하기)
    주어진 배열을 시계방향으로 90도 회전시켜서 출력하는 프로그램을 완성하시오.
    
    class Exercise5_9 {
    public static void main(String[] args) {
    char[][] star = {
    {'*','*',' ',' ',' '},
    {'*','*',' ',' ',' '},
    {'*','*','*','*','*'},
    {'*','*','*','*','*'}
    };
    char[][] result = new char[star[0].length][star.length];
    for(int i=0; i < star.length;i++) {
    for(int j=0; j < star[i].length;j++) {
    System.out.print(star[i][j]);
    }
    System.out.println();
    }
    System.out.println();
    for(int i=0; i < star.length;i++) {
    for(int j=0; j < star[i].length;j++) {
    /*
    (1) . 알맞은 코드를 넣어 완성하시오
    */
    }
    }
    for(int i=0; i < result.length;i++) {
    for(int j=0; j < result[i].length;j++) {
    System.out.print(result[i][j]);
    }
    System.out.println();
    }
    } // end of main
    } // end of class
    
    [ 실행결과 ]
    **
    **
    *****
    *****
    
    ****
    ****
    **
    **
    **

---
+++ JavaTest69.java
@@ -0,0 +1,84 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.NavigableSet;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import javax.sound.midi.Instrument;
+import javax.sound.midi.MidiEvent;
+
+public class JavaTest69 {
+
+	public static void main(String[] args) {
+		// 1. 90도 회전하기
+//		char[][] star = {
+//				{'*','*',' ',' ',' '},
+//				{'*','*',' ',' ',' '},
+//				{'*','*','*','*','*'},
+//				{'*','*','*','*','*'}
+//				};
+//
+//		// 회전한 값을 넣을 배열 생성
+//		char[][] result = new char[star[0].length][star.length];
+//		
+//		for (int i = 0; i < star.length; i++) {
+//			for (int j = 0; j < star[i].length; j++) {
+//				System.out.print(star[i][j]);
+//			}
+//			System.out.println();
+//		}
+//		System.out.println();
+		
+		// 90도 회전한 값을 result 배열에 넣고 출력
+//		for (int i = 0; i < result.length; i++) {
+//			for (int j = 0; j < result[i].length; j++) {
+//				result[i][j] = star[3-j][i];
+//				System.out.print(result[i][j]);
+//			}
+//			System.out.println();
+//		}
+//		
+//		System.out.println();
+		
+		// 2. 180도 회전한 값을 result2 배열에 넣고 출력
+//		char[][] result2 = new char[star.length][star[0].length];
+//		
+//		for (int i = 0; i < result2.length; i++) {
+//			for (int j = 0; j < result2[i].length; j++) {
+//				result2[i][j] = star[3-i][j];
+//				System.out.print(result2[i][j]);
+//			}
+//			System.out.println();
+//		}
+		
+		
+		
+		// 3. 270도 회전한 값을 result3 배열에 넣고 출력
+//		char[][] result3 = new char[star[0].length][star.length];
+//		
+//		for (int i = 0; i < result3.length; i++) {
+//			for (int j = 0; j < result3[i].length; j++) {
+//				result3[i][j] = star[j][4-i];
+//				System.out.print(result3[i][j]);
+//			}
+//		System.out.println();
+//		}
+	}
+}
