2918edefbf7d701f469a14c93cfa10d96d330c6d
Shim <91776742+Shimcoding@users.noreply.github.com>
2022-02-12 16:04:50 +0900

    java_jungsuk3_연습문제 7-27(클래스에 있는 value 값 얻어 내기)
    다음과 같은 실행결과를 얻도록 코드를 완성하시오.
    
    class Outer {
    int value=10;
    class Inner {
    int value=20;
    void method1() {
    int value=30;
    System.out.println(/* (1) */);
    System.out.println(/* (2) */);
    System.out.println(/* (3) */);
    }
    } // Inner클래스의 끝
    } // Outer클래스의 끝
    class Exercise7_27 {
    public static void main(String args[]) {
    /*
    (4) . 알맞은 코드를 넣어 완성하시오
    */
    inner.method1();
    }
    }
    
    [ 실행결과] 
    30
    20
    10

---
+++ JavaTest89.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class JavaTest89 {
+
+	public static void main(String[] args) {
+		// 각각의 value 값 얻어 내기
+		Outer1 outer = new Outer1();
+		Outer1.Inner inner = outer.new Inner();
+		inner.method1();
+	}
+}
+
+class Outer1{
+	int value = 10;
+	
+	class Inner{
+		int value = 20;
+		void method1() {
+			int value = 30;
+			
+			// value = 30;
+			System.out.println(value);
+			// value = 20;
+			System.out.println(this.value);
+			// value = 10;
+			System.out.println(Outer1.this.value);
+		}
+	}
+}
